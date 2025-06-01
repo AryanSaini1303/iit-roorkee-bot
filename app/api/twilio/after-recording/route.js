63c77561e4707f4312a7b35a1ad5bde9aeceb76b
Aryan Saini <saini.aryan9999@gmail.com>
2025-06-01 07:19:34 +0530

    refactor: optimize language handling and media response in Twilio after-recording route

--- app/api/twilio/after-recording/route.js
+++ app/api/twilio/after-recording/route.js
@@ -11,14 +11,13 @@ export async function POST(req) {
   const response = new twiml.VoiceResponse();
   const callSid = form.get('CallSid');
   const recordingUrl = form.get('RecordingUrl');
+  const client = await getRedisClient();
 
   // silence leads to no recording url so we hangup the call with a thankyou note
   // console.log(recordingUrl);
   const transcript = await GetTranscription(recordingUrl);
   if (!transcript || transcript.trim().length < 3) {
-    const client = await getRedisClient();
     const lang = await client.get(`lang:${callSid}`);
-    console.log(lang);
     await appendMessage(callSid, {
       role: 'system',
       content:
@@ -48,14 +47,20 @@ export async function POST(req) {
     }
   } catch (err) {}
   if (isEndCall) {
+    const lang = await client.get(`lang:${callSid}`);
     await appendMessage(callSid, {
       role: 'system',
-      content: 'Thank you for your response',
+      content:
+        lang === 'English'
+          ? 'Thank you for your response'
+          : 'Terima kasih atas maklum balas anda',
     });
-    response.play(
-      'https://x8kvogjfhjihtrrx.public.blob.vercel-storage.com/thankYou-Ns1PPq5miOzZ1BT13wTTN2h8dONtZy.mp3',
-    );
-    const client = await getRedisClient();
+    const media =
+      lang === 'English'
+        ? 'https://x8kvogjfhjihtrrx.public.blob.vercel-storage.com/thankYou-Ns1PPq5miOzZ1BT13wTTN2h8dONtZy.mp3'
+        : 'https://x8kvogjfhjihtrrx.public.blob.vercel-storage.com/thankYou2-5RNIAreL6uKs03wYvgHNklrwyr65qG.mp3';
+    response.play(media);
+    // const client = await getRedisClient();
     await client.del(`voiceId:${callSid}`);
     response.hangup();
     return new Response(response.toString(), {
