086d33a14b934d885c3d4a5c75c775d5c4999fae
Aryan Saini <saini.aryan9999@gmail.com>
2025-07-25 00:33:49 +0530

    Feat: added loader for loading chat

---
+++ front-end/components/loader.js
@@ -0,0 +1,14 @@
+export default function LoaderComponent() {
+  return (
+    <div className="dot-spinner">
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+    </div>
+  );
+}
