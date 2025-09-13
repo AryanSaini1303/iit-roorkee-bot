3d377b28d2a4c6f76420837fa3e1ccccd24ff1a1
Aryan Saini <saini.aryan9999@gmail.com>
2025-09-13 14:10:21 +0530

    Feat: enhance CategoriesPieChart with legends and style adjustments

--- front-end/components/CategoriesCountChart.js
+++ front-end/components/CategoriesCountChart.js
@@ -6,14 +6,10 @@ import styles from './CategoriesCountChart.module.css';
 const COLORS = ['#6366f1', '#a855f7', '#06b6d4', '#f97316', '#10b981'];
 
 export default function CategoriesPieChart({ data }) {
-//   console.log(data);
+  //   console.log(data);
   return (
     <div className={styles.chartContainer}>
-      <ResponsiveContainer
-        width="100%"
-        height={300}
-        style={{ outline: 'none' }}
-      >
+      <ResponsiveContainer width="30%" height={350} style={{ outline: 'none' }}>
         <PieChart>
           <Pie
             data={data}
@@ -24,8 +20,10 @@ export default function CategoriesPieChart({ data }) {
             outerRadius={120}
             innerRadius={105}
             paddingAngle={0}
+            labelLine={false}
             label={({ name, percent }) =>
-              `${name} ${(percent * 100).toFixed(0)}%`
+              // `${name} ${(percent * 100).toFixed(0)}%`
+              `${(percent * 100).toFixed(0)}%`
             }
           >
             {data.map((_, index) => (
@@ -35,16 +33,17 @@ export default function CategoriesPieChart({ data }) {
                 stroke="rgba(255, 255, 255, 0.472)"
                 strokeWidth={2}
                 style={{
-                //   filter: `drop-shadow(0px 0px 1px ${
-                //     COLORS[index % COLORS.length]
-                //   }`,
+                  filter: `drop-shadow(1px 1px 0.5px ${
+                    COLORS[index % COLORS.length]
+                  }`,
                   outline: 'none',
                 }}
               />
             ))}
           </Pie>
           <Tooltip
-            formatter={(value, name) => [`${value} queries`, name]}
+            // formatter={(value, name) => [`${value} queries`, name]}
+            formatter={(value, name) => [`${value} queries`]}
             contentStyle={{
               background: '#fff',
               border: '1px solid #ddd',
@@ -53,6 +52,23 @@ export default function CategoriesPieChart({ data }) {
           />
         </PieChart>
       </ResponsiveContainer>
+      <ul className={styles.legends}>
+        <li>
+          Casual <span></span>
+        </li>
+        <li>
+          Document-Specific <span></span>
+        </li>
+        <li>
+          Cross-Document <span></span>
+        </li>
+        <li>
+          Contextual <span></span>
+        </li>
+        <li>
+          Meta <span></span>
+        </li>
+      </ul>
     </div>
   );
 }
