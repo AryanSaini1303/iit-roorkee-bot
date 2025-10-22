79ec8b40030acf8a0b1ea55138f05fe7bbf9f1fb
Aryan Saini <saini.aryan9999@gmail.com>
2025-10-22 18:51:21 +0530

    Feat: implemented auto login after existing session check

--- front-end/app/CWC/page.js
+++ front-end/app/CWC/page.js
@@ -4,9 +4,14 @@ import MaintenancePage from '@/components/notFound';
 import styles from './page.module.css';
 import { createClient } from '@/utils/supabase/client';
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import SessionLoader from '@/components/SessionLoader';
 
 export default function Home() {
   const supabase = createClient('CWC');
+  const [session, setSession] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
   const signIn = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
@@ -25,36 +30,60 @@ export default function Home() {
     }
   };
 
+  useEffect(() => {
+    const getSession = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      setSession(data?.session ?? null);
+      if (data?.session) {
+        router.push('/CWC/home');
+      }
+      // console.log(data?.session);
+      setLoading(false);
+      if (error) {
+        console.error('Session fetch error:', error.message);
+      }
+    };
+    getSession();
+  }, []);
+
   // if(true){
   //   return <MaintenancePage/>
   // }
 
   return (
     <div className={`wrapper ${styles.container}`}>
-      <div className={styles.imageContainer}>
-        <img
-          src="/images/loginImage.jpg"
-          alt="ai-assistant"
-          className={styles.image}
-        />
-      </div>
-      <section className={styles.loginSection}>
-        <div className={styles.holder}>
-          <h1>DamChat</h1>
-        </div>
-        <div className={styles.holder}>
-          <div className={styles.infoContainer}>
-            {/* <p>&ndash; by ICED, IIT Roorkee</p> */}
-            <img src="/images/icedLogo.png" alt="rocket" />
-          </div>
-        </div>
-        <div className={styles.holder}>
-          <button className={styles.signInBtn} onClick={signIn}>
-            <img src="/images/googleLogo.png" alt="google logo" />
-            <h3>Sign in with Google</h3>
-          </button>
-        </div>
-      </section>
+      {loading ? (
+        <SessionLoader />
+      ) : (
+        !session && (
+          <>
+            <div className={styles.imageContainer}>
+              <img
+                src="/images/loginImage.jpg"
+                alt="ai-assistant"
+                className={styles.image}
+              />
+            </div>
+            <section className={styles.loginSection}>
+              <div className={styles.holder}>
+                <h1>DamChat</h1>
+              </div>
+              <div className={styles.holder}>
+                <div className={styles.infoContainer}>
+                  {/* <p>&ndash; by ICED, IIT Roorkee</p> */}
+                  <img src="/images/icedLogo.png" alt="rocket" />
+                </div>
+              </div>
+              <div className={styles.holder}>
+                <button className={styles.signInBtn} onClick={signIn}>
+                  <img src="/images/googleLogo.png" alt="google logo" />
+                  <h3>Sign in with Google</h3>
+                </button>
+              </div>
+            </section>
+          </>
+        )
+      )}
     </div>
   );
 }
