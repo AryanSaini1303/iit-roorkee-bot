655616294f53a083d51e1899bf835c828026bdab
AryanSaini1303 <saini.aryan9999@gmail.com>
2026-02-24 11:11:35 +0530

    refactor: user fetching logic to incorporate total user count

--- front-end/app/api/list_users/route.js
+++ front-end/app/api/list_users/route.js
@@ -3,6 +3,22 @@ import { createClient } from '@supabase/supabase-js';
 
 export async function POST(req) {
   const { origin } = await req.json();
+  const fetchAllUsers = async (supabase) => {
+    let allUsers = [];
+    let page = 1;
+    const perPage = 100;
+    while (true) {
+      const { data, error } = await supabase.auth.admin.listUsers({
+        page,
+        per_page: perPage,
+      });
+      if (error) throw error;
+      allUsers.push(...data.users);
+      if (page >= data.lastPage) break;
+      page++;
+    }
+    return allUsers;
+  };
   const supabase = createClient(
     origin === 'CWC'
       ? process.env.SUPABASE_SERVER_URL_CWC
@@ -12,18 +28,16 @@ export async function POST(req) {
       : process.env.SUPABASE_SERVICE_ROLE_KEY_DSA,
   );
   try {
-    const { data, error } = await supabase.auth.admin.listUsers();
-    if (error) throw error;
-    // console.log(data);
-    const users = data.users.map((u) => ({
+    const usersData = await fetchAllUsers(supabase);
+    const users = usersData.map((u) => ({
       id: u.id,
       email: u.email,
       created_at: u.created_at,
       last_sign_in_at: u.last_sign_in_at,
-      name: u.user_metadata.full_name,
-      phone: u.user_metadata.phone,
-      organisation: u.user_metadata.organisation,
-      designation: u.user_metadata.designation,
+      name: u.user_metadata?.full_name || null,
+      phone: u.user_metadata?.phone || null,
+      organisation: u.user_metadata?.organisation || null,
+      designation: u.user_metadata?.designation || null,
     }));
     return NextResponse.json({ users });
   } catch (err) {
